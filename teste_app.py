480204da2b270c4ac458912c918ea8aced1a6c0d
alisonsergio2025 <alisonsergio@yahoo.com.br>
2025-02-09 20:11:19 -0300

    Update teste_app.py

--- teste_app.py
+++ teste_app.py
@@ -1,16 +1,18 @@
+import streamlit as st
+
 st.write("teste")
 
 lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
 
-# Adicionando um item à lista
-st.write(lista_de_compras.append("Ovos"))
+# Adicionando um item à lista corretamente
+lista_de_compras.append("Ovos")
+st.write("Item adicionado: Ovos")
 
 # Removendo um item da lista
 lista_de_compras.remove("Banana")
+st.write("Item removido: Banana")
 
-# Exibindo a lista
-print("Lista de Compras:")
-for item in lista_de_compras:
-    print("- " + item)
-
+# Exibindo a lista formatada
+st.write("Lista de Compras Atualizada:")
 st.write(lista_de_compras)
+
