c013dc5403268644b1b532f336408810fc7f9afa
alisonsergio2025 <alisonsergio@yahoo.com.br>
2025-02-09 20:08:03 -0300

    Update teste_app.py

--- teste_app.py
+++ teste_app.py
@@ -1,13 +1,3 @@
-#nome = 'ALISON'
-#print(f'Meu nome é : {nome}')
-#lista = [1,'olá mundo',True,9.7]
-#tupla = (1,'olá mundo',True,9.7)
-# Criando uma lista de compras
-import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-
 st.write("teste")
 
 lista_de_compras = ["Maçã", "Banana", "Leite", "Pão", "Queijo"]
